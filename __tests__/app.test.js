0e8b8b1462c87555158439fceacc2b1b00f2db29
AaronMullan <aaroncmullan@gmail.com>
2020-02-17 14:16:51 -0800

    post and get routes working

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const Note = require('../lib/models/Note');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -14,7 +15,60 @@ describe('app routes', () => {
     return mongoose.connection.dropDatabase();
   });
 
+  let note;
+  beforeEach(async() => {
+    note = await Note.create({
+      title: 'bulk test title',
+      text: 'bulk test text'
+    });
+  });
+
   afterAll(() => {
     return mongoose.connection.close();
   });
+
+  it('can create a new note', () => {
+    return request(app)
+      .post('/api/v1/notes')
+      .send({
+        title: 'test title',
+        text: 'test text'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          title: 'test title',
+          text: 'test text',
+          __v: 0
+        });
+      });
+  });
+  it('gets a nte by id', () => {
+    return request(app)
+      .get(`/api/v1/notes/${note.id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          title: 'bulk test title',
+          text: 'bulk test text',
+          __v: 0
+        });
+      });
+  });
+  it('gets all notes', () => {
+    return request(app)
+      .get('/api/v1/notes')
+      .then(res => {
+        expect(res.body).toEqual([
+          {
+            _id: expect.any(String),
+            title: 'bulk test title',
+            text: 'bulk test text',
+            __v: 0
+          }
+        ]);
+      });
+  });
+  it('updates a note')
 });
+ 
